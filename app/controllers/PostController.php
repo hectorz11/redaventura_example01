432f360bcda37bdd3ccc7b6e76c814bd13b99d62
hector <hector.rz11@gmail.com>
2015-09-14 20:01:14 -0500

    hectorz11: method post and put ...

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -6,10 +6,40 @@ class PostController extends \BaseController {
 	{
 		if (Input::has('title')) {
 			$title = Input::get('title');
-
 			$posts = Post::where('title','like','%'.$title.'%')->take(5)->get();
-
 			return Response::json(['data' => $posts]);
 		}
-	}	
+	}
+
+	
+
+	public function store()
+	{
+		$answer = Post::createPost(Input::all());
+		if ($answer['error'] == true) {
+			return Response::json(['message' => $answer['message']]);
+		} else {
+			return Response::json(['message' => $answer['message']]);
+		}
+	}
+
+	public function show($id)
+	{
+		$post = Post::find($id);
+		if ($post) {
+			return Response::json(['data' => $post]);
+		} else {
+			return Response::json(['error' => true, 'description' => 'no existe!']); 
+		}
+	}
+
+	public function update($id)
+	{
+		$answer = Post::updatePost(Input::all(), $id);
+		if ($answer['error'] == true) {
+			return Response::json(['message' => $answer['message']]);
+		} else {
+			return Response::json(['message' => $answer['message']]);
+		}
+	}
 }
--- app/models/Post.php
+++ app/models/Post.php
@@ -12,4 +12,69 @@ class Post extends Eloquent {
 	{
 		return $this->belongTo('User', 'user_id');
 	}
+
+	public static function createPost($input)
+	{
+		$answer = [];
+		$rules = [
+			'title' => 'required',
+			'body' => 'required',
+			'user_id' => 'required|integer',
+		];
+		$validation = Validator::make($input, $rules);
+
+		if ($validation->fails()) {
+			$answer['message'] = $validation->errors()->getMessages();
+			$answer['error'] = true;
+		} else {
+			$post = new Post;
+			$post->title = Input::get('title');
+			$post->image_url = Input::get('image_url');
+			$post->body = Input::get('body');
+			$post->user_id = Input::get('user_id');
+			$post->status = 1;
+
+			if ($post->save()) {
+				$answer['message'] = 'Creado con exito!';
+				$answer['error'] = false;
+				$answer['data'] = $post;
+			} else {
+				$answer['message'] = 'CREATE error, team noob!';
+				$answer['error'] = false;
+			}
+		}
+		return $answer;
+	}
+
+	public static function updatePost($input, $id)
+	{
+		$answer = [];
+		$rules = [
+			'title' => 'required',
+			'body' => 'required',
+			'user_id' => 'required|integer',
+		];
+		$validation = Validator::make($input, $rules);
+
+		if ($validation->fails()) {
+			$answer['message'] = $validation->errors()->getMessages();
+			$answer['error'] = true;
+		} else {
+			$post = Post::find($id);
+			$post->title = Input::get('title');
+			$post->image_url = Input::get('image_url');
+			$post->body = Input::get('body');
+			$post->user_id = Input::get('user_id');
+
+			if ($post->save()) {
+				$answer['message'] = 'Editado con exito!';
+				$answer['error'] = false;
+				$answer['data'] = $post;
+			} else {
+				$answer['message'] = 'UPDATE error, team noob!';
+				$answer['error'] = false;
+			}
+		}
+		return $answer;
+	}
 }
